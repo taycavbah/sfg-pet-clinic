68b5c097334db7d8e71de3e558e4396f01d1d984
tay cavett <taycavett98@gmail.com>
2022-12-07 17:30:10 -0600

    added Specialty class and added specialties to vet closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity{
+    
+    private String discription;
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+}
